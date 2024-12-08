b67e9475b6250ef7365a19aeda2cf2fbbd5f0aeb
Elconme <92789004+Elconme@users.noreply.github.com>
2024-12-08 23:24:52 +0100

    Delete wsgi.py

--- wsgi.py
+++
@@ -1,7 +0,0 @@
-from IncendiosForestales import app 
-
-if __name__ == "__main__":
-  from gunicorn.app.wsgiapp import WSGIApplication
-  application = WSGIApplication()
-  application.wsgi_app = app
-  application.run()
